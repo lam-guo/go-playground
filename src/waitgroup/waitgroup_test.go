a652644cb64863208f55139470797483df33920a
lam-guo <824430898@qq.com>
2021-07-21 23:34:13 +0800

    学习waitgroup

--- src/waitgroup/waitgroup_test.go
+++ src/waitgroup/waitgroup_test.go
@@ -3,6 +3,7 @@ package waitgroup
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -39,4 +40,55 @@ func TestWg(t *testing.T) {
 	}
 	time.Sleep(time.Second * 10)
 	wg.Done()
+}
+
+func TestWg2(t *testing.T) {
+	var wg sync.WaitGroup
+	var counter int32
+	n := 2000
+	for i := 0; i < n; i++ {
+		go func(i int) {
+			wg.Add(1)
+			atomic.AddInt32(&counter, 1) // 存在wait()提前触发问题
+			// counter += 1 // Dangerous!!
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	fmt.Println(counter)
+	for i := 0; i < 2; i++ {
+		time.Sleep(1 * time.Second)
+		fmt.Println(counter)
+	}
+}
+
+func TestWg3(t *testing.T) {
+	wg := sync.WaitGroup{}
+	var counter int
+	var other int32
+	hashMap := sync.Map{}
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		other += 1
+		// go func(wg sync.WaitGroup, num int) { // 注意这里传入wg是值传递，相当于新的wg,新的wg一直在Done，但是旧的wg一直在Add
+		go func(wg *sync.WaitGroup, num int) {
+			hashMap.Store(counter, true)
+			counter += 1
+			wg.Done()
+		}(&wg, i)
+	}
+	// hashMap.Range(func(key, value interface{}) bool {
+	// 	fmt.Println(key)
+	// 	return true
+	// })
+	for i := 0; i < 1000; i++ {
+		if v, ok := hashMap.Load(i); !ok {
+			fmt.Println(i)
+		} else {
+			fmt.Println(v, "ok")
+		}
+	}
+	wg.Wait()
+	fmt.Println(counter, "----")
+	fmt.Println(other, "-----")
 }
