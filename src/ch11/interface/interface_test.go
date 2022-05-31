9eaf3cc7d736f2ad4723e7f286b7dda534d23434
guohualin <824430898@qq.com>
2022-05-31 17:47:35 +0800

    feat(interface): 空接口，带有类型信息就不是nil了

--- src/ch11/interface/interface_test.go
+++ src/ch11/interface/interface_test.go
@@ -1,6 +1,7 @@
 package interface_test
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -21,4 +22,18 @@ func TestInterface(t *testing.T) {
 	t.Log(p.WriteHelloWorld())
 }
 
+// 带有类型信息就不是nil了
+func TestInterface2(t *testing.T) {
+	var x interface{} = nil
+	var y *int = nil
+	interfaceIsNil(x)
+	interfaceIsNil(y)
+}
 
+func interfaceIsNil(x interface{}) {
+	if x == nil {
+		fmt.Println("empty interface")
+		return
+	}
+	fmt.Println("non-empty interface")
+}
