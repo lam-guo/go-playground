4f5da19e3515b9d2815738c940b4f85c1c1dfde0
guohualin <824430898@qq.com>
2022-12-30 10:43:25 +0800

    feat(algorithm): subset算法

--- src/playground/log_test.go
+++ src/playground/log_test.go
@@ -14,3 +14,48 @@ func TestXxx(t *testing.T) {
 	})
 	log.Errorf("%+v\n", err)
 }
+
+//mqtt事件
+type MQTTEvent struct {
+}
+
+// 设备事件
+type DeviceEvent struct {
+}
+
+type IotDevice interface {
+	// mqtt处理器
+	MQTTHandler(event MQTTEvent)
+	// 设备处理器
+	DeviceHandler(event DeviceEvent)
+	// 执行器（自由业务处理）
+	Processor()
+}
+
+type Sw7 struct {
+}
+
+func (s Sw7) MQTTHandler(event MQTTEvent) {
+}
+func (s Sw7) DeviceHandler(event DeviceEvent) {
+}
+func (s Sw7) Processor() {
+}
+
+func start(d IotDevice) {
+	for {
+		mqtt_event := mqtt_recv()
+		device_event := device_recv()
+		d.Processor()
+		d.MQTTHandler(mqtt_event)
+		d.DeviceHandler(device_event)
+	}
+}
+
+func mqtt_recv() MQTTEvent {
+	return MQTTEvent{}
+}
+
+func device_recv() DeviceEvent {
+	return DeviceEvent{}
+}
